8400184b790d1b9faf020d6ffcf30a2b76d842e4
axrez <isdragon112@gmail.com>
2019-02-18 08:21:02 +0100

    Enabled start-stop functionallity

--- Arduino/WebServer/WebServer.ino
+++ Arduino/WebServer/WebServer.ino
@@ -4,7 +4,7 @@
 #include <ESP8266mDNS.h>
 
 #ifndef STASSID
-#define STASSID "diller"
+#define STASSID "Rotinmaskin"
 #define STAPSK  "12345678"
 #endif
 
@@ -13,19 +13,40 @@ const char* password = STAPSK;
 
 ESP8266WebServer server(80);
 
-const int led = 13;
+const int led = 2;
+
+bool running = false;
 
 void handleRoot() {
-  digitalWrite(led, 1);
-  server.send(200, "text/html", "<!DOCTYPE html> <html lang='en'> <head> <meta charset='UTF-8' /> <meta name='viewport' content='width=device-width, initial-scale=1.0' /> <meta http-equiv='X-UA-Compatible' content='ie=edge' /> <!-- <link rel='stylesheet' href='style.css' /> --> <title>M&M's sortering</title> <style> * { font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; } h1 { font-size: 1.7rem; font-weight: 300; } body { display: flex; flex-direction: column; align-items: center; } .header { text-transform: uppercase; } button { padding: 0 2rem; border: 0; cursor: pointer; border-radius: 3px; text-transform: uppercase; } .start { background: rgb(0, 185, 0); color: white; } .stop { background: rgb(124, 0, 0); color: white; } .btn-area > button { margin: 0.5rem; } </style> </head> <body> <section class='head'> <h1 class='header'>Kontrolpanel</h1> <div class='divider'></div> </section> <section class='main'> <div class='btn-area'> <button onclick='handleStart()' class='start' ><p>start</p></button> <button class='stop'><p>stop</p></button> </div> </section> <section class='footer'><h6>test 2</h6></section> </body> <script> const btn = document.getElementsByClassName('start'); const handleStart = () => { fetch('192.168.4.1') .then(res => console.log('start')) } </script> </html>");
+  String root = "<!DOCTYPE html> <html lang='en'> <head> <meta charset='UTF-8' /> <meta name='viewport' content='width=device-width, initial-scale=1.0' /> <meta http-equiv='X-UA-Compatible' content='ie=edge' /> <!-- <link rel='stylesheet' href='style.css' /> --> <title>M&M's sortering</title> <style> * { font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; } h1 { font-size: 1.7rem; font-weight: 300; } body { display: flex; flex-direction: column; align-items: center; } .header { text-transform: uppercase; } button { padding: 0 2rem; border: 0; cursor: pointer; border-radius: 3px; text-transform: uppercase; } .start { background: rgb(0, 185, 0); color: white; } .stop { background: rgb(124, 0, 0); color: white; } .btn-area > div > button { margin: 0.5rem; } .info-area { text-align: center; } .first-row { padding-left: 1rem; } </style> </head> <body> <section class='head'> <h1 class='header'>Kontrolpanel</h1> <div class='divider'></div> </section> <section class='main'> <div class='info-area'> <div id='state'></div> <div id='speed'></div> </div> <div class='btn-area'> <div class='first-row'> <button onclick='handleStart()' class='start' ><p>start</p></button> <button class='stop' onclick='handleStop()' ><p>stop</p></button> </div> <div class='second-row'> <button class='speedUp' onclick='handleSpeedUp()' ><p>Speed +</p></button> <button class='speedDown' onclick='handleSpeedDown()' ><p>Speed -</p></button> </div> </div> </section> <section class='footer'><h6>test 2</h6></sect/ion> </body> <script> let started = false; let speed = 0; const stateField = document.getElementById('state'); stateField.innerHTML = started ? 'Started' : 'Stopped'; const handleStart = () => { fetch('/start') .then(res => res.status === 206 ? started = true : null); stateField.innerHTML = started ? 'Stopped' : 'Started'; }; const handleStop = () => { fetch('/stop') .then(res => res.status === 207 ? started = false : null); stateField.innerHTML = started ? 'Stopped' : 'Started'; }; const handleSpeedUp = () => { fetch('/speedUp') .then(res => console.log(res.status)); }; const handleSpeedDown = () => { fetch('/speedDown') .then(res => console.log(res.status)); }; </script> </html>";
   digitalWrite(led, 0);
+  server.send(200, "text/html", root);
+  digitalWrite(led, 1);
 }
 
 void handleStart() {
   Serial.println("start");
-  server.send(200);
+  server.send(206, "text/plain", "1");
+  running = true;
+  digitalWrite(led, 0);
 }
 
+void handleStop() {
+  Serial.println("stop");
+  server.send(207, "text/plain", "0");
+  running = false;
+  digitalWrite(led, 1);
+}
+
+void handleSpeedUp() {
+  Serial.println("Speed Up");
+  server.send(210);
+}
+
+void handleSpeedDown() {
+  Serial.println("Speed Down");
+  server.send(211);
+}
 
 void handleNotFound() {
   digitalWrite(led, 1);
@@ -60,9 +81,11 @@ void setup(void) {
 
   server.on("/start", handleStart);
 
-  server.on("/inline", []() {
-    server.send(200, "text/plain", "this works as well");
-  });
+  server.on("/stop", handleStop);
+
+  server.on("/speedUp", handleSpeedUp);
+
+  server.on("/speedDown", handleSpeedDown);
 
   server.onNotFound(handleNotFound);
 
